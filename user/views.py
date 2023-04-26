e7aa69c2e9080bc63fb1daaefd76d9fb89de1ff1
Kushal-27 <kushalpoudel2058@gmail.com>
2023-04-26 09:49:16 +0545

    Template fix

--- user/views.py
+++ user/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 # Create your views here.
 def index(request):
-    return render(request, 'homee.html')
+    return render(request, 'index.html')
 
 def login(request):
     if request.method=='POST':
